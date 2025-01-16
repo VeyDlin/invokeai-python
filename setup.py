8feed1c152ecf3509ecea8120e9fb5bdaac85d9e
Kyle Nekto <nekto@veydlin.com>
2025-01-16 23:54:40 +0300

    up v

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="invokeai-python",
-    version="0.1.0",
+    version="0.2.1",
     description="InvokeAI API package",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
